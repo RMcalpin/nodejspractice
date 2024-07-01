0110e44b1aad5df7fe30a0e680a56c1e83814c37
RMcalpin <rmcalpin@emich.edu>
2024-07-01 15:20:46 -0400

    added first html file

--- petstore/routes/index.js
+++ petstore/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+
+router.get('/', function (req, res, next) {
+  fs.readFile('petstore.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 });
 
 module.exports = router;
