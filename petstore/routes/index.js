2cbaf4f397a9ff86b0039586576be66facc66497
RMcalpin <rmcalpin@emich.edu>
2024-07-05 14:05:47 -0400

    second step

--- petstore/routes/index.js
+++ petstore/routes/index.js
@@ -1,17 +1,22 @@
 var express = require('express');
-var fs = require('fs');
 var router = express.Router();
-const app = express()
-const port = 3010
 
 /* GET home page. */
 //router.get('/', function(req, res, next) {
 //  res.render('index', { title: 'Express' });
 //});
 
+const sections = [
+  {id:0, section1:"Intro to Programming"},
+  {id:1, section2:"Data Structures"},
+  {id:2, section3:"Computer Organization I"},
+  {id:3, section4:"Internet-based Programming"},
+  {id:4, section5:"Algorithms and Data Structures"},
+]
+
 router.get('/', function (req, res, next) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("Hello from /");
+  res.write("<h1 style='color:green; text-align:center;'>Sections</h1>");
   return res.end();
 });
 
--- petstore/routes/instructors.js
+++ petstore/routes/instructors.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const instructors = [
+  {id:0, instructor1:"Zhang"},
+  {id:1, instructor2:"Maniccam"},
+  {id:2, instructor3:"Bahorski"},
+  {id:3, instructor4:"Evett"},
+  {id:4, instructor5:"Panja"},
+]
+
 /* GET users listing. */
 router.get('/instructors', function(req, res, next) {
   res.send('hello from instructors');
