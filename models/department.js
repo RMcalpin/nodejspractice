d89682e6375062f539eb1e4e9013090f05249aaa
ReedM99 <rmcalpin@emich.edu>
2025-04-02 19:20:16 -0400

    finished Objection.js

--- models/department.js
+++ models/department.js
@@ -1,8 +1,21 @@
-const { Model } = require('objection');
+const { Model, ref } = require('objection');
 
 class Department extends Model {
   static get tableName() {
     return 'department'; // Make sure this matches your actual table name
+  }
+
+  static get relationMappings() {
+    return {
+      course: {
+        relation: Model.HasManyRelation,
+        modelClass: Course,
+        join: {
+          from: 'persons.id',
+          to: 'animals.ownerId'
+        }
+      },
+    };
   }
 }
 
